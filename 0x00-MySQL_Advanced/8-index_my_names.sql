849790a615b9223f11d0d24323d52ff9a0d96403
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-04-25 21:52:28 +0100

    8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,3 +1,2 @@
--- Task 8: Optimize simple search - creates an index idx_name_first
--- on the table names and the first letter of name
+--Task 8: Create an index named "idx_name_first" on the table "names" for optimizing simple searches based on the first letter of the name
 CREATE INDEX idx_name_first ON names ( name(1) );
