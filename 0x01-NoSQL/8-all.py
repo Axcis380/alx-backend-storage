a7b0f432ee3789a82a29c7a6ce41cc682a3d98dc
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-04-30 00:22:35 +0100

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" Performing MongoDB Operations in Python using pymongo """
 
 
 def list_all(mongo_collection):
-    """ List all documents in Python """
+    """ Retrieve all documents using Python """
     documents = mongo_collection.find()
 
     if documents.count() == 0:
