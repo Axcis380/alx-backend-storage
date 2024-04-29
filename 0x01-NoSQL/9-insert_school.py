cc04a2ddd00768fedd80b24afcdddba0a3d0a172
Axcis380 <133226284+Axcis380@users.noreply.github.com>
2024-04-30 00:23:59 +0100

    9-insert_school.py

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" MongoDB Operations with Python using pymongo """
+""" Performing MongoDB operations in Python using pymongo """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """ Inserts a new document in a collection based on kwargs """
+    """ Add a new document to a collection using kwargs """
     document_id = mongo_collection.insert(kwargs)
     return document_id
