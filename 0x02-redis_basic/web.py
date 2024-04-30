1ac4e388753e11394a758fc10703c4906f53d686
root <anas.baidouri@gmail.com>
2024-04-30 21:01:55 +0000

    test2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,34 @@
 #!/usr/bin/env python3
 """
-Implémente un cache web expirant et un suivi
+Implements an expiring web cache and tracker
 """
 from typing import Callable
 from functools import wraps
 import redis
 import requests
-
 redis_client = redis.Redis()
 
+
 def url_count(method: Callable) -> Callable:
-    """Compte combien de fois une URL est consultée"""
+    """counts how many times an url is accessed"""
     @wraps(method)
     def wrapper(*args, **kwargs):
         url = args[0]
         redis_client.incr(f"count:{url}")
-        cached = redis_client.get(url)
+        cached = redis_client.get(f'{url}')
         if cached:
             return cached.decode('utf-8')
-        content = method(*args, **kwargs)
-        redis_client.setex(url, 10, content)
-        return content
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
     return wrapper
+
 
 @url_count
 def get_page(url: str) -> str:
-    """Obtient une page et met en cache la valeur"""
+    """get a page and cache value"""
     response = requests.get(url)
     return response.text
+
 
 if __name__ == "__main__":
     get_page('http://slowwly.robertomurray.co.uk')
