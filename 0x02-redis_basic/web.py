748c5f3209ec0ceb18081803ce1623f0ed4a9d83
root <anas.baidouri@gmail.com>
2024-04-30 21:00:12 +0000

    test2

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,33 @@
 #!/usr/bin/env python3
-
-import requests
-import time
+"""
+Implémente un cache web expirant et un suivi
+"""
+from typing import Callable
 from functools import wraps
+import redis
+import requests
 
-CACHE = {}
+redis_client = redis.Redis()
 
-def track_access_count(func):
-    @wraps(func)
-    def wrapper(url):
-        count_key = f"count:{url}"
-        if count_key in CACHE:
-            CACHE[count_key] += 1
-        else:
-            CACHE[count_key] = 1
-        return func(url)
+def url_count(method: Callable) -> Callable:
+    """Compte combien de fois une URL est consultée"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(url)
+        if cached:
+            return cached.decode('utf-8')
+        content = method(*args, **kwargs)
+        redis_client.setex(url, 10, content)
+        return content
     return wrapper
 
-def cache_result(func):
-    @wraps(func)
-    def wrapper(url):
-        if url in CACHE and time.time() - CACHE[url]['timestamp'] < 10:
-            return CACHE[url]['content']
-        else:
-            content = func(url)
-            CACHE[url] = {'content': content, 'timestamp': time.time()}
-            return content
-    return wrapper
-
-@track_access_count
-@cache_result
-def get_page(url):
+@url_count
+def get_page(url: str) -> str:
+    """Obtient une page et met en cache la valeur"""
     response = requests.get(url)
     return response.text
 
-# Exemple d'utilisation
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/http://www.example.com"
-    print(get_page(url))
-    print(get_page(url))
-    print(get_page(url))
-    print(f"Nombre d'accès pour {url}: {CACHE.get('count:' + url, 0)}")
+    get_page('http://slowwly.robertomurray.co.uk')
